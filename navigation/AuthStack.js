72e0f1153525dc8515aa89fe2c87117b7d0792e6
Dharmik7 <dholariyadharmik7@gmail.com>
2022-11-07 23:56:01 -0700

    disabled going back to login screen

--- navigation/AuthStack.js
+++ navigation/AuthStack.js
@@ -1,34 +1,36 @@
-import React from 'react'
-import {View, Text} from 'react-native'
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
-import OnboardingScreen from '../screens/OnboardingScreen';
-import HomeScreen from '../screens/HomeScreen';
-import LoginScreen from '../screens/LoginScreen';
-import RegisterScreen from '../screens/RegisterScreen';
-import VerifyScreen from '../screens/VerifyScreen';
+import React from "react";
+import { View, Text } from "react-native";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import OnboardingScreen from "../screens/OnboardingScreen";
+import HomeScreen from "../screens/HomeScreen";
+import LoginScreen from "../screens/LoginScreen";
+import RegisterScreen from "../screens/RegisterScreen";
+import VerifyScreen from "../screens/VerifyScreen";
 
 const screens = {
   OnboardingScreen: {
     screen: OnboardingScreen,
-    
   },
   LoginScreen: {
     screen: LoginScreen,
-    headerShown: false
+    headerShown: false,
   },
   RegisterScreen: {
     screen: RegisterScreen,
-    headerShown: false
+    headerShown: false,
   },
   VerifyScreen: {
     screen: VerifyScreen,
-    headerShown: false
+    headerShown: false,
   },
 };
 
-const AuthStack = createStackNavigator(screens,{defaultNavigationOptions:{
-  headerShown:false
-}});
+const AuthStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerShown: false,
+    gestureEnabled: false,
+  },
+});
 
 export default createAppContainer(AuthStack);
